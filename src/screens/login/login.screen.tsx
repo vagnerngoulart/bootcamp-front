92bbcd619cc95453bff2f650958c26de1b88d83c
vagnerngoulart <vagnerngoulart@hotmail.com>
2021-12-21 19:49:53 -0300

    Altera estilos e logo

--- src/screens/login/login.screen.tsx
+++ src/screens/login/login.screen.tsx
@@ -2,17 +2,22 @@
 import { Container, Grid } from "@mui/material";
 import Button from "../../components/buttons/button/button.component";
 import InputText from "../../components/inputs/input-text/input-text-component";
+import login from '../../assets/icons/logo.svg';
+import {Logo, Wrapper} from './login.styled';
 export default function Login(){
     return (
         <>
         <Container>
-        <Grid container justifyContent={'center'}>
-            <Grid item xs={4}>
-                <InputText type={'text'} placeholder={'E-mail'}/>
-                <InputText type={'password'} placeholder={'Senha'}/>
-                <Button primary={true}>Entrar</Button>
-            </Grid>
-        </Grid>
+            <Wrapper container justifyContent={'center'}>
+                <Grid item xs={3} justifyContent={'center'}>
+                    <Grid container justifyContent={'center'}>
+                        <Logo src={login} alt={'Netflix Logo'}/>
+                        <InputText type={'text'} placeholder={'E-mail'}/>
+                        <InputText type={'password'} placeholder={'Senha'}/>
+                        <Button primary={true}>Entrar</Button>
+                    </Grid>
+                </Grid>
+            </Wrapper>
         </Container>
       
         </>
---
+++ src/screens/login/login.styled.ts
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import styled from "styled-components";
+
+export const Wrapper = styled(Grid)`
+    min-height: 100vh;
+    align-items: center;
+`
+
+export const Logo = styled.img`
+    margin: 0 0 128px;
+
+`
